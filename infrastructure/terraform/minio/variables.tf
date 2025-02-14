847b171e23ddf1b0045c75dd49ac80238e56a0e7
Ben Wal <ben@benwal.io>
2025-02-14 14:07:11 -0500

    ✨ feat(minio): added to tfm

---
+++ infrastructure/terraform/minio/variables.tf
@@ -0,0 +1,19 @@
+variable "onepassword_sa_token" {
+  type        = string
+  description = "OP Service Account Token"
+  sensitive   = true
+  default     = null
+}
+
+variable "service_account_json" {
+  type        = string
+  description = "The path to the service account JSON for OnePassword."
+  sensitive   = true
+  default     = null
+}
+
+variable "minio_url" {
+  type        = string
+  description = "Minio Server URL"
+  default     = "s3.benwal.rodeo"
+}
