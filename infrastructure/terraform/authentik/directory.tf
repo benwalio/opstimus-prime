36508e1cad7ef15eaa6f89220dbfb6bc8f7ef821
Ben Wal <ben@benwal.io>
2025-02-15 07:54:26 -0500

    👷 build(tfm): working on imports

--- infrastructure/terraform/authentik/directory.tf
+++ infrastructure/terraform/authentik/directory.tf
@@ -48,15 +48,15 @@ resource "authentik_source_ldap" "lldap" {
   base_dn       = module.onepassword_lldap.fields["LLDAP_AK_BASE_DN"]
 
   property_mappings = [
-    "authentik default LDAP Mapping: mail",
-    "authentik default LDAP Mapping: Name",
-    "authentik default Active Directory Mapping: givenName",
-    "authentik default Active Directory Mapping: sn",
-    "authentik default OpenLDAP Mapping: cn",
-    "authentik default OpenLDAP Mapping: uid"
+    "1a6d6daf-b3f2-420b-b422-6d1b5cd1e278",
+    "448bf5d7-2e43-47d2-a5a4-9663fa185760",
+    "d1734f42-0f1a-4385-9fa6-44c5f51c86f3",
+    "c102e661-ac40-4c89-b93c-552de1c5b3d9",
+    "087d519b-89a1-42a0-9117-584313b68efb",
+    "63348598-ec6f-4d04-8587-73dcfa23db7f"
   ]
   property_mappings_group = [
-    "authentik default OpenLDAP Mapping: cn"
+    "087d519b-89a1-42a0-9117-584313b68efb"
   ]
 
   user_path_template      = "LDAP/users"
