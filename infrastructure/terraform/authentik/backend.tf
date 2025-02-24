f74899a38d30783a060abc9f1e9f2c0f6874aee2
ben wal <13092287+benwalio@users.noreply.github.com>
2025-02-24 11:41:06 -0500

    🐛 fix(tfm): updated bucket region

--- infrastructure/terraform/authentik/backend.tf
+++ infrastructure/terraform/authentik/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "terraform-state"
     key    = "authentik/authentik.tfstate"
-    region = "main" # Region validation will be skipped
+    region = "us-east-1" # Region validation will be skipped
 
     endpoints = {
       s3 = "https://s3.benwal.rodeo" # Minio endpoint
